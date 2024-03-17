7136bddd454cbc0a838fc5d94f6cf80c2f02fe7a
bombies <xjamesdzn@gmail.com>
2024-03-17 12:35:04 -0500

    Setup neovim for React projects

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,12 @@
 return {
   {
+    "nvimtools/none-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "configs.null-ls"
+    end,
+  },
+  {
     "stevearc/conform.nvim",
     config = function()
       require "configs.conform"
@@ -11,5 +18,73 @@ return {
     opts = {
       git = { enable = true },
     },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "typescript-language-server",
+        "tailwindcss-language-server",
+        "json-lsp",
+        "html-lsp",
+        "prettierd",
+        "stylua",
+        "eslint-lsp"
+      }
+    }
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    ft = {
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact"
+    },
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end
+  },
+  {
+    "nvm-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- defaults 
+        "vim",
+        "lua",
+        "vimdoc",
+
+        -- web dev 
+        "html",
+        "css",
+        "scss",
+        "javascript",
+        "typescript",
+        "tsx",
+
+        -- other
+        "python",
+
+        -- low level
+        "c",
+      }
+    }
+  },
+  {
+    "roobert/tailwindcss-colorizer-cmp.nvim",
+    lazy = false,
+    config = function()
+      require("tailwindcss-colorizer-cmp").setup({})
+      require("configs.tailwindcss-colorizer-cmp")
+    end
   },
 }
